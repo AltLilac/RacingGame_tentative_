59ed2d4d6540d0005ed1391a87ece97bd9934201
hetima333 <hetima333@gmail.com>
2021-05-15 15:43:34 +0900

    Minimap.csとSpeedMeter.csをUniRx化した

--- RacingGame_tentative_/Assets/Scripts/SpeedMeter.cs
+++ RacingGame_tentative_/Assets/Scripts/SpeedMeter.cs
@@ -1,29 +1,23 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
-public class SpeedMeter : MonoBehaviour {
-	[SerializeField]
-	private float _speed = 0.0f;
+public class SpeedMeter : MonoBehaviour
+{
+	void Start()
+	{
+		var speedText = GetComponentInChildren<TextMeshProUGUI>();
 
-	[SerializeField]
-	private TextMeshProUGUI _speedText;
-
-	// Start is called before the first frame update
-	void Start() {
-		_speedText = GetComponentInChildren<TextMeshProUGUI>();
-	}
-
-	// Update is called once per frame
-	void Update() {
-		_speed = CarController.GetSpeed();
-
-		if (_speed > 1000) {
-			_speed = 999;
-		}
 		// スピードメーターの数値を更新する
-		_speedText.text = Math.Floor(_speed).ToString();
+		this.UpdateAsObservable()
+			.Select(_ => CarController.GetSpeed())
+			.Select(spd => Math.Floor(spd))
+			.Select(spd => spd > 1000 ? 999 : spd)
+			.Subscribe(spd =>
+			{
+				speedText.text = spd.ToString();
+			});
 	}
 }
