07d74a4821656975248adf24d5689dface8c12d3
AltLilac <49546834+AltLilac@users.noreply.github.com>
2021-05-31 07:56:38 +0900

    CarController.cs の書き換えが完了した
    ついでにコードの整形と、コメントも追加した。

--- RacingGame_tentative_/Assets/Scripts/Car/CarController.cs
+++ RacingGame_tentative_/Assets/Scripts/Car/CarController.cs
@@ -2,34 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 
 public class CarController : MonoBehaviour
 {
     private const string _Horizontal = "Horizontal";
     private const string _Vertical = "Vertical";
 
+	// Inputs
     private float _horizontalInput;
     private float _verticalInput;
-    private float _currentSteerAngle;
-    private float _currentbreakForce;
-    private bool _isBreaking;
 
-    [SerializeField] private float motorForce;
-    [SerializeField] private float breakForce;
-    [SerializeField] private float maxSteerAngle;
+    private float _currentSteerAngle;	
+    private float _currentBreakForce;	
 
-    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    private bool _isBreaking;	// ブレーキボタンを押しているか
+
+    [SerializeField] private float motorForce;		// 車のパワー
+    [SerializeField] private float breakForce;      // ブレーキ力
+	[SerializeField] private float maxSteerAngle;   // ステア角
+
+	// タイヤのコライダー
+	[SerializeField] private WheelCollider frontLeftWheelCollider;
     [SerializeField] private WheelCollider frontRightWheelCollider;
     [SerializeField] private WheelCollider rearLeftWheelCollider;
     [SerializeField] private WheelCollider rearRightWheelCollider;
 
+	// タイヤの位置
     [SerializeField] private Transform frontLeftWheelTransform;
     [SerializeField] private Transform frontRightWheeTransform;
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
-    private static float _currentSpeed;
-    private Rigidbody _rb;
+    private static float _currentSpeed;		// HUD シーンへ送る速度の情報
+
+	private Rigidbody _rb;	// 速度計算用 Rigidbody
 
     // m/s から km/h へ変換
     private const float _ConvertValue = 3.6f;
@@ -37,21 +45,23 @@ public class CarController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+		this.FixedUpdateAsObservable()
+			.Subscribe(_ =>
+			{
+				GetInput();
+
+				HandleMotor();
+
+				HandleSteering();
+
+				UpdateWheels();
+
+				CalculateSpeed();
+			});
     }
 
-    private void FixedUpdate()
-    {
-        GetInput();
-
-        HandleMotor();
-
-        HandleSteering();
-
-        UpdateWheels();
-
-		CalculateSpeed();
-    }
-
+	// HUD シーンへ速度の情報を送る
     public static float GetSpeed()
     {
         return _currentSpeed;
@@ -75,23 +85,25 @@ public class CarController : MonoBehaviour
     {
         frontLeftWheelCollider.motorTorque = _verticalInput * motorForce;
         frontRightWheelCollider.motorTorque = _verticalInput * motorForce;
-        _currentbreakForce = _isBreaking ? breakForce : 0f;
+
+        _currentBreakForce = _isBreaking ? breakForce : 0f;
         ApplyBreaking();
     }
 
     // ブレーキ
     private void ApplyBreaking()
     {
-        frontRightWheelCollider.brakeTorque = _currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = _currentbreakForce;
-        rearLeftWheelCollider.brakeTorque = _currentbreakForce;
-        rearRightWheelCollider.brakeTorque = _currentbreakForce;
+        frontRightWheelCollider.brakeTorque = _currentBreakForce;
+        frontLeftWheelCollider.brakeTorque = _currentBreakForce;
+        rearLeftWheelCollider.brakeTorque = _currentBreakForce;
+        rearRightWheelCollider.brakeTorque = _currentBreakForce;
     }
 
     // ハンドル回転
     private void HandleSteering()
     {
         _currentSteerAngle = maxSteerAngle * _horizontalInput;
+
         frontLeftWheelCollider.steerAngle = _currentSteerAngle;
         frontRightWheelCollider.steerAngle = _currentSteerAngle;
     }
